32a6d7002e8633a47ff0a7048b6640c99099005a
onadj <oliver.nad@gmail.com>
2023-11-29 13:30:49 +0000

    dsa

--- manage.py
+++ manage.py
@@ -1,27 +1,6 @@
 
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-import platform
-
-# Check if the platform is Windows
-if platform.system() == "Windows":
-    try:
-        # Attempt to import pywin32
-        import pywin32
-    except ImportError:
-        # Handle the case where pywin32 is not available on non-Windows platforms
-        pywin32 = None
-else:
-    # Set pywin32 to None for non-Windows platforms
-    pywin32 = None
-
-# Now you can use pywin32 if it's available (and you're on Windows)
-if pywin32:
-    # Your Windows-specific code using pywin32
-    pass
-else:
-    # Handle the case where pywin32 is not available on non-Windows platforms
-    pass
 
 import os
 import sys
